70e6f91417ffcc3809594027530203ef9bb70dbe
younus-always <younusofficial443@gmail.com>
2026-06-22 15:46:01 +0600

    zod: create zodSchema for register user and created reusable higher order validateRequest function to validate zodSchema

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { userCreateZodSchema } from "./user.validate";
 
 export const userRoute = Router();
 
-userRoute.post("/", userController.createUser);
+userRoute.post("/",
+      validateRequest(userCreateZodSchema),
+      userController.createUser
+);
 userRoute.get("/:userId", userController.getUserById);
 userRoute.delete("/:userId", userController.deleteUser);
 userRoute.get("/", userController.getAllUser);
